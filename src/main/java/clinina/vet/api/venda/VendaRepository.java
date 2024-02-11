aad41f4c735e59cef3b07f9aba4e286f995ed74b
Sergio Rocha <sergiorsj1@hotmail.com>
2024-02-11 00:46:28 -0300

    atualiza o banco

--- src/main/java/clinina/vet/api/venda/VendaRepository.java
+++ src/main/java/clinina/vet/api/venda/VendaRepository.java
@@ -8,6 +8,6 @@ public interface VendaRepository extends JpaRepository<Venda, Long> {
     //@Query(value = "SELECT v.id, p.imagemP, p.produto, v.quantidade, v.preco_unitario as precoUnitario, v.preco_total as PrecoTotal, v.data FROM vendas v JOIN produtos p ON v.produto_id = p.id;", nativeQuery = true)
    //List<DadosItensVendidos> buscarItensVendidos();
 
-    @Query(value = "SELECT v.id, v.peso, p.imagemP, p.produto, v.quantidade, v.preco_unitario as precoUnitario, v.preco_total as PrecoTotal, v.data FROM vendas v JOIN produtos p ON v.produto_id = p.id where data >= '2024-02-05 00:00:00' and data <= '2024-02-05 23:59:59';", nativeQuery = true)
+    @Query(value = "SELECT v.id, v.peso, p.imagemP, p.produto, v.quantidade, v.preco_unitario as precoUnitario, v.preco_total as PrecoTotal, v.data FROM vendas v JOIN produtos p ON v.produto_id = p.id where data >= '2024-02-09 00:00:00' and data <= '2024-02-09 23:59:59';", nativeQuery = true)
     List<DadosItensVendidos> buscarItensVendidos();
 }
