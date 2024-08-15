e98087a354acb449e7f2b15be42665f07f1d6dfe
Sergio Rocha <sergiorsj1@hotmail.com>
2024-08-15 01:38:55 -0300

    caixa controller fechar caixa edit

--- src/main/java/clinina/vet/api/caixa/CaixaRepository.java
+++ src/main/java/clinina/vet/api/caixa/CaixaRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface CaixaRepository extends JpaRepository<Caixa, Long> {
 
-    @Query(value = "SELECT c.id, c.abertura_data, c.abertura_valor, c.despesas_caixa, c.entrada, c.fechamento_caixa_data, c.fechamento_caixa_valor, c.credito_conferido, c.debito_conferido, c.dinheiro_conferido, c.pix_conferido, c.fiado_conferido FROM caixa c where abertura_data >= CONCAT(:ano, '-', :mes, '-', :dia, ' 00:00:00') and abertura_data <= CONCAT(:ano, '-', :mes, '-', :dia, ' 23:59:59');", nativeQuery = true)
+    @Query(value = "SELECT c.id, c.abertura_data, c.abertura_valor, c.despesas_caixa, c.entrada, c.fechamento_caixa_data, c.fechamento_caixa_valor, c.credito_conferido, c.debito_conferido, c.dinheiro_conferido, c.pix_conferido, c.fiado_conferido, c.status FROM caixa c where abertura_data >= CONCAT(:ano, '-', :mes, '-', :dia, ' 00:00:00') and abertura_data <= CONCAT(:ano, '-', :mes, '-', :dia, ' 23:59:59');", nativeQuery = true)
     Caixa buscarCaixa(@Param("dia") int dia, @Param("mes") int mes, @Param("ano") int ano);
 
 }
--- src/main/java/clinina/vet/api/controller/CaixaController.java
+++ src/main/java/clinina/vet/api/controller/CaixaController.java
@@ -4,10 +4,12 @@ import clinina.vet.api.caixa.Caixa;
 import clinina.vet.api.caixa.CaixaDTO;
 import clinina.vet.api.caixa.CaixaRepository;
 import clinina.vet.api.venda.DadosItensVendidos;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("caixa")
@@ -28,8 +30,28 @@ public class CaixaController {
 
     //Mudar, não é mais save e criar o abricaxia
     @PutMapping("/fecharCaixa")
+    @Transactional
     public void fecharCaixa(@RequestBody CaixaDTO caixaDTO) {
+        Optional<Caixa> caixa = this.caixaRepository.findById(caixaDTO.id());
+        Caixa caixaEncontrado = new Caixa();
+        if (caixa.isPresent()) {
+            caixaEncontrado = caixa.get();
+        }
+        caixaEncontrado.setEntrada(caixaDTO.entrada());
+        caixaEncontrado.setFechamento_caixa_data(caixaDTO.fechamento_caixa_data());
+        caixaEncontrado.setFechamento_caixa_valor(caixaDTO.fechamento_caixa_valor());
+        caixaEncontrado.setCredito_conferido(caixaDTO.credito_conferido());
+        caixaEncontrado.setDebito_conferido(caixaDTO.debito_conferido());
+        caixaEncontrado.setDinheiro_conferido(caixaDTO.dinheiro_conferido());
+        caixaEncontrado.setPix_conferido(caixaDTO.pix_conferido());
+        caixaEncontrado.setFiado_conferido(caixaDTO.fiado_conferido());
+        caixaEncontrado.setStatus(caixaDTO.status());
+    }
+
+    @PostMapping("/abrirCaixa")
+    public void abrirCaixa(@RequestBody CaixaDTO caixaDTO) {
         Caixa caixa = new Caixa(caixaDTO);
         this.caixaRepository.save(caixa);
     }
+
 }
